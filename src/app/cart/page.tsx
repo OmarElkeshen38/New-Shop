3d2ae38dd03e47d39853597488e82d86ebc33311
omarelkeshen <oelkeshen5@gmail.com>
2023-11-28 08:15:39 +0200

    edit the cart page to be responsive

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -38,21 +38,19 @@ const CartPage: FC<CartPageProps> = ({}) => {
     localStorage.setItem('cart', JSON.stringify(updatedCart));
   };
 
-
-
   const displayProducts = () => {
     return cartProducts.map((product: Product) => (
       <div
         key={product.id}
-        className="flex items-center justify-between border-b border-gray-300 p-4"
+        className="flex flex-col md:flex-row items-center justify-between border-b border-gray-300 p-4"
       >
-        <div className="card card-side p-3 w-2xl bg-slate-100 shadow-xl">
-          <figure>
+        <div className="card card-side p-3 w-full md:w-2/3 bg-slate-100 shadow-xl mb-4 md:mb-0">
+          <figure className="mb-4 md:mb-0 md:mr-4">
             <Image
-              className="rounded-md w-36"
+              className="rounded-md w-full md:w-36"
               src={`${product.image}`}
               alt={`product ${product.title}`}
-              width={250} 
+              width={250}
               height={200}
             />
           </figure>
@@ -74,8 +72,8 @@ const CartPage: FC<CartPageProps> = ({}) => {
   };
 
   return (
-    <div className="container flex my-6">
-      <div className="w-3/4">
+    <div className="container flex flex-col md:flex-row my-6">
+      <div className="w-full md:w-3/4">
         <h1 className="text-4xl mb-6">Your Cart:</h1>
         {cartProducts.length === 0 ? (
           <p>Your cart is empty.</p>
@@ -83,25 +81,23 @@ const CartPage: FC<CartPageProps> = ({}) => {
           <div>{displayProducts()}</div>
         )}
       </div>
-      <div className="w-1/4 ml-4">
-        <div className="sticky top-0">
-          <div className="p-4 border border-gray-300 rounded-md bg-slate-100">
-            <h2 className="text-2xl font-semibold mb-4">Checkout</h2>
-            {cartProducts.length > 0 && (
-              <>
-                <div className="flex justify-between mb-4">
-                  <p className="text-xl font-semibold">Total:</p>
-                  <p className="text-xl">${calculateTotalPrice()}</p>
-                </div>
-                <button
-                  onClick={handleCheckout}
-                  className="btn btn-block bg-slate-600 text-slate-100 hover:bg-slate-500"
-                >
-                  Proceed to Checkout
-                </button>
-              </>
-            )}
-          </div>
+      <div className="w-full md:w-1/4 mt-4 md:ml-4">
+        <div className="p-4 border border-gray-300 rounded-md bg-slate-100">
+          <h2 className="text-2xl font-semibold mb-4">Checkout</h2>
+          {cartProducts.length > 0 && (
+            <>
+              <div className="flex justify-between mb-4">
+                <p className="text-xl font-semibold">Total:</p>
+                <p className="text-xl">${calculateTotalPrice()}</p>
+              </div>
+              <button
+                onClick={handleCheckout}
+                className="btn btn-block bg-slate-600 text-slate-100 hover:bg-slate-500"
+              >
+                Proceed to Checkout
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
